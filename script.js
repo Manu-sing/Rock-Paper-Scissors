f4edad2cbb9316661475a8bebb1bbb3f6a9a706a
Emanuele Guarnaccia <emanueleguarnaccia@gmail.com>
2021-11-06 16:35:03 +0800

    worked on the second function on script.js. now it works but still not added the input.value from the user

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
-const gameValues = ["Rock", "Paper", "Scissors"];
 
-function computerPlay() {
-    return gameValues[Math.floor(Math.random() * gameValues.length)];
-}
+const forma = document.querySelector('form');
+const inp = document.querySelector('input');
+const para = document.querySelector('p');
 
-console.log(computerPlay());
+
+
+
+        let choices = ['Rock', 'Paper', 'Scissors'];
+        
+        let randomValue  = (function computerPlay() {
+
+            let randomChoice = choices[Math.floor(Math.random() * choices.length)];
+            
+            para.outerHTML += " " + randomChoice;
+
+            return randomChoice;
+
+        }) ();
+
+
+
+        function playRound(playerSelection, computerSelection) {
+                    if (playerSelection == "rock" && computerSelection == choices[0]) {
+                        return "It's a tie! Play again!";
+                    } else if (playerSelection == "rock" && computerSelection == choices[1]) {
+                        return "You lose! Paper beats rock.";
+                    } else if (playerSelection == "rock" && computerSelection == choices[2]) {
+                        return "You win! Rock beats scissors."; 
+                    } else if (playerSelection == "paper" && computerSelection == choices[0]) {
+                        return "You win! Paper beats rock.";
+                    } else if (playerSelection == "paper" && computerSelection == choices[1]) {
+                        return "It's a tie! Play again!";
+                    } else if (playerSelection == "paper" && computerSelection == choices[2]) {
+                        return "You lose! Scissors beat paper.";
+                    } else if (playerSelection == "scissors" && computerSelection == choices[0]) {
+                        return "You lose! Rock beats scissors.";
+                    } else if (playerSelection == "scissors" && computerSelection == choices[1]) {
+                        return "You win! Scissors beat paper.";
+                    } else if (playerSelection == "scissors" && computerSelection == choices[2]) {
+                        return "It's a tie! Play again!";
+                    } else return "I think you forgot how to play this game!"
+        }
+
+        let playerSelection = "rock" /*document.getElementById('user-inp').value.toLowerCase();*/
+        let computerSelection = randomValue;
+
+        
+
+        console.log(playRound(playerSelection, computerSelection));
