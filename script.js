7f58d696c81d0e0ed706c04d4a2917fc79506511
Emanuele Guarnaccia <emanueleguarnaccia@gmail.com>
2021-11-11 15:25:25 +0800

    script.js is almost done, still some small issues on Chrome and still not perfectly finalised with the if statement when the user presses 'cancel' or 'esc'

--- script.js
+++ script.js
@@ -9,75 +9,67 @@ const out4 = document.getElementById('computer-score');
 const out5 = document.getElementById('final-result');
 
 
-        // get a ramdom choice between Rock-paper-scissor from Computer
+// get a ramdom choice between Rock-paper-scissor from Computer
 
-        let choices = ['rock', 'paper', 'scissors'];
-        
-        let randomValue  = function computerPlay() {
+let choices = ['rock', 'paper', 'scissors'];
+let userScore = 0;
+let computerScore = 0;
 
-            let randomChoice = choices[Math.floor(Math.random() * choices.length)];
-            
-            out1.textContent = randomChoice;
+let randomValue = function computerPlay() {
+    let randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    out1.textContent = randomChoice;
+    return randomChoice;
+};
 
-            return randomChoice;
 
-        };
 
-       
-
-    function game() {
-
-        let userScore = 0;
-        let computerScore = 0;
-
-        // iterate the function playRound 5 times with a for loop
-        for (let i = 0; i < 5; i++) {
+async function game() {
+    // iterate the function playRound 5 times with a for loop
+    for (let i = 0; i < 5; i++) {
         // prompt an initial message "Rock, paper, scissor" to ask the user to input one of the 3
         let sign = prompt("Rock, paper or scissors?");
 
         // use a function to compare the random choice from computer with what the user has input in the prompt and return a result
         function playRound(playerSelection, computerSelection) {
-         
-            
-          if (playerSelection == "rock" && computerSelection == choices[0]) {
-            i = i -1;    
-            return "It's a tie! Play again!";
-                
+            if (playerSelection == "rock" && computerSelection == choices[0]) {
+                i = i - 1;
+                return "It's a tie! Play again!";
 
-         } else if (playerSelection  == "rock" && computerSelection == choices[1]) {
-                
+
+            } else if (playerSelection == "rock" && computerSelection == choices[1]) {
+
                 computerScore = computerScore + 1;
                 out4.textContent = computerScore;
                 // this is when the computer wins
-                if(computerScore == 3) {
+                if (computerScore == 3) {
                     i = 5; // so that it practically breaks out of the loop
-                    out5.textContent = "Unfortunately you lost the game :("   
+                    out5.textContent = "Unfortunately you lost the game :("
                 } else return "You lose this round! Paper beats rock.";
 
-            } else if (playerSelection  == "rock" && computerSelection == choices[2]) {
-                
+            } else if (playerSelection == "rock" && computerSelection == choices[2]) {
+
                 userScore = userScore + 1;
                 out3.textContent = userScore;
-                if(userScore == 3) {
+                if (userScore == 3) {
                     i = 5;
                     out5.textContent = "Congratulations, you won the game!"
                 } else return "You win this round! Rock beats scissors.";
 
-            } else if (playerSelection  == "paper" && computerSelection == choices[0]) {
-                
+            } else if (playerSelection == "paper" && computerSelection == choices[0]) {
+
                 userScore = userScore + 1;
                 out3.textContent = userScore;
-                if(userScore == 3) {
+                if (userScore == 3) {
                     i = 5;
                     out5.textContent = "Congratulations, you won the game!"
                 } else return "You win this round! Paper beats rock.";
 
-            } else if (playerSelection  == "paper" && computerSelection == choices[1]) {
-                i = i -1; 
+            } else if (playerSelection == "paper" && computerSelection == choices[1]) {
+                i = i - 1;
                 return "It's a tie! Play again!";
 
-            } else if (playerSelection  == "paper" && computerSelection == choices[2]) {
-                
+            } else if (playerSelection == "paper" && computerSelection == choices[2]) {
+
                 computerScore = computerScore + 1;
                 out4.textContent = computerScore;
                 if (computerScore == 3) {
@@ -85,8 +77,8 @@ const out5 = document.getElementById('final-result');
                     out5.textContent = "Unfortunately you lost the game :("
                 } else return "You lose this round! Scissors beat paper.";
 
-            } else if (playerSelection  == "scissors" && computerSelection == choices[0]) {
-                
+            } else if (playerSelection == "scissors" && computerSelection == choices[0]) {
+
                 computerScore = computerScore + 1;
                 out4.textContent = computerScore;
                 if (computerScore == 3) {
@@ -94,59 +86,55 @@ const out5 = document.getElementById('final-result');
                     out5.textContent = "Unfortunately you lost the game :("
                 } else return "You lose this round! Rock beats scissors.";
 
-            } else if (playerSelection  == "scissors" && computerSelection == choices[1]) {
-                
+            } else if (playerSelection == "scissors" && computerSelection == choices[1]) {
+
                 userScore = userScore + 1;
                 out3.textContent = userScore;
-                if(userScore == 3) {
+                if (userScore == 3) {
                     i = 5;
                     out5.textContent = "Congratulations, you won the game!"
                 } else return "You win this round! Scissors beat paper.";
 
-            } else if (playerSelection  == "scissors" && computerSelection == choices[2]) {
-                i = i -1; 
+            } else if (playerSelection == "scissors" && computerSelection == choices[2]) {
+                i = i - 1;
                 return "It's a tie! Play again!";
-            // this currently doesn't work as it tries to convert the playerSelection toLowerCase, as requested in the let playerSelection = sign.toLowerCase(), but it cannot because the sign is null and so it returns an error that sign is null
-            } else if(playerSelection === '' || playerSelection === null) {
+                // this currently doesn't work as it tries to convert the playerSelection toLowerCase, as requested in the let playerSelection = sign.toLowerCase(), but it cannot because the sign is null and so it returns an error that sign is null
+            } else if (playerSelection === '' || playerSelection === null) {
                 i = 0;
-                 
+
+                /* if (!sign) {
+                    i -= 1;
+                    continue;
+                  }*/
+
             } else {
-                i = i -1; 
-                return "I think you forgot how to play this game!"; 
-            }    
-
-
+                i = i - 1;
+                return "I think you forgot how to play this game!";
             }
-            // store the playerSelection argument value (equal to user input made lower case) in a variable
-            let playerSelection = sign.toLowerCase();
-            // store the computerSelection argument value (equal to radom choice determined by function randomValue) in a variable
-            let computerSelection = randomValue();
-
-            // print the user input made to lower case in the paragraph tag
-            out0.textContent = sign;
-            // print the return statement from the function in the out2 tag
-            out2.textContent= playRound(playerSelection, computerSelection); 
-
         }
 
+        // store the playerSelection argument value (equal to user input made lower case) in a variable
+        let playerSelection = sign.toLowerCase();
+        // store the computerSelection argument value (equal to radom choice determined by function randomValue) in a variable
+        let computerSelection = randomValue();
 
-        }
+        // print the user input made to lower case in the paragraph tag
+        out0.textContent = sign;
+        // print the return statement from the function in the out2 tag
+        out2.textContent = playRound(playerSelection, computerSelection);
 
-   // run the function
-   game();
+        await promisifiedRequestAnimationFrame();
+    }
+}
 
-   
+function promisifiedRequestAnimationFrame() {
+    return new Promise((resolve, reject) => {
+        window.requestAnimationFrame(() => {resolve()});
+    })
+}
 
-   
-
-   
-
-
-        
-
-
-
-        
+// run the function
+game();
                 
         
         
